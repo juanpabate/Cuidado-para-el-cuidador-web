d2d4cd84a4903343a978635096622c9bcfbcff10
Juan Pablo Benjumea <juanpabate318@hotmail.com>
2023-08-11 21:13:17 -0500

    se realizo la seccion Tecnologia con los querys de Desktop de escritorio

---
+++ script.js
@@ -0,0 +1,63 @@
+//MODAL 1
+const modalButon1 = document.getElementById('tecnologia1');
+const modal1= document.getElementById('modal1');
+const modalClose1= document.getElementById('modalClose1');
+
+modalButon1.addEventListener('click', ()=>{
+    modal1.classList.add('showModal');
+});
+modalClose1.addEventListener('click', ()=>{
+    modal1.classList.remove('showModal');
+})
+
+
+//MODAL 2
+const modalButon2 = document.getElementById('tecnologia2');
+const modal2= document.getElementById('modal2');
+const modalClose2= document.getElementById('modalClose2');
+
+modalButon2.addEventListener('click', ()=>{
+    modal2.classList.add('showModal');
+});
+modalClose2.addEventListener('click', ()=>{
+    modal2.classList.remove('showModal');
+})
+
+
+//MODAL 3
+const modalButon3 = document.getElementById('tecnologia3');
+const modal3= document.getElementById('modal3');
+const modalClose3= document.getElementById('modalClose3');
+
+modalButon3.addEventListener('click', ()=>{
+    modal3.classList.add('showModal');
+});
+modalClose3.addEventListener('click', ()=>{
+    modal3.classList.remove('showModal');
+})
+
+//MODAL 4
+const modalButon4 = document.getElementById('tecnologia4');
+const modal4= document.getElementById('modal4');
+const modalClose4= document.getElementById('modalClose4');
+
+modalButon4.addEventListener('click', ()=>{
+    modal4.classList.add('showModal');
+});
+modalClose4.addEventListener('click', ()=>{
+    modal4.classList.remove('showModal');
+})
+
+
+//MODAL 5
+const modalButon5 = document.getElementById('tecnologia5');
+const modal5= document.getElementById('modal5');
+const modalClose5= document.getElementById('modalClose5');
+
+modalButon5.addEventListener('click', ()=>{
+    modal5.classList.add('showModal');
+});
+modalClose5.addEventListener('click', ()=>{
+    modal5.classList.remove('showModal');
+})
+
